ec2cc2bf8c06d2dafda69724bec272dfea9203a5
salmanarrahman <salmanarrahman@gmail.com>
2023-12-17 12:52:19 +0600

    learning vue

---
+++ src/views/CityView.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <p>{{ setCity }}</p>
+    <p>{{ searchText }}</p>
+    
+</template>
+<script>
+
+    export default {
+        data() {
+    return {
+        searchText: null,
+    };
+    },
+    computed:{
+        setCity () {
+            this.searchText = this.$route.params.name;
+            return this.$route.params.name;
+        }
+    }
+    
+    
+
+};
+
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {}
+  },
+  daisyui: {
+    themes: ['light', 'dark', 'retro']
+  },
+  plugins: [require('daisyui')]
+}
